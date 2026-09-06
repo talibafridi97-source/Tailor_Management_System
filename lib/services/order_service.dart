a9dacefd37dbdf3970438bec4f488d90e83048ee
LENOVO <talibafridi97@gmail.com>
2026-09-05 23:38:24 -0700

    Update server IP address to 192.168.10.5 and synchronize all API services

--- lib/services/order_service.dart
+++ lib/services/order_service.dart
@@ -3,8 +3,8 @@ import 'package:http/http.dart' as http;
 import 'auth_service.dart';
 
 class OrderService {
-  // Local IP updated to 192.168.10.7
-  static const String baseUrl = 'http://192.168.10.7:5000/api/orders';
+  // Local IP updated to 192.168.10.5
+  static const String baseUrl = 'http://192.168.10.5:5000/api/orders';
 
   static Future<Map<String, dynamic>> createOrder({
     required String clientName,
